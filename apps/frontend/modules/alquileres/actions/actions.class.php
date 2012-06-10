8fb4128b1d95b05bec747fd95ffc655cf4326bb5
sebastian <seba_rw84@hotmail.com>
2012-06-09 22:30:19 -0300

    - configurada hoja de estilo para modulo de alquiler, y comenzada su presentacion y logica

--- apps/frontend/modules/alquileres/actions/actions.class.php
+++ apps/frontend/modules/alquileres/actions/actions.class.php
@@ -17,6 +17,76 @@ class alquileresActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
+      
+    $nombre_dias = array('lunes','martes','miercoles','jueves','viernes','sabado','domingo');
+
     //$this->forward('default', 'module');
+    $this->valores_cancha = array (
+        1=>"cancha5 1.jpg",
+        2=>"cancha5 2.jpg",
+        3=>"cancha8.jpg"
+    );  
+    
+    //vector donde se guardan los horarios disponibles correspondiente al dia de la semana
+    $semana = array(
+        'lunes'=> array(),
+        'martes' => array(),
+        'miercoles' => array(),
+        'jueves' => array(),
+        'viernes' => array(),
+        'sabado' => array(),
+        'domingo' => array()
+    );
+    
+    $hora_inicio=8;
+    $hora_fin=20;
+    //asigno a cada dia los horarios disponibles, consideramos que cada dia tiene un horario corrido de 8:00 a 20:00 hs
+    foreach ($semana as $dia=>$horarios) {
+      for ($i=$hora_inicio;$i<=$hora_fin;$i++){
+        $horarios[]=$i;
+      }
+      $semana[$dia]=$horarios;
+    }
+    
+    //tomo dia y hora actuales
+    $num_dia=date('N');
+    $num_hora=date('H');   
+    
+    $this->dia_limite=8;
+   
+    $this->fecha=array();
+    $this->horario=array(array());
+    
+    for($i=0;$i<$this->dia_limite;$i++)
+    {  
+        $j=$i+$num_dia-1;//indice para el dia de la semana correspondiente;
+        
+        if($i!=0)
+        {
+            $this->horario[$i]=$semana[$nombre_dias[$j%7]];
+        }
+        else
+        {
+            if($num_hora <$hora_fin)
+            {
+                      for ($k=$num_hora+1;$k<=$hora_fin;$k++)
+                      {
+                        $this->horario[$i][]=$k;
+                      }
+            }
+
+        }                
+        
+        $this->fecha[$i]=$nombre_dias[$j%7]." ".$this->calcularfecha($i);
+        
+        //$nombre_dias[$j%7]." ".calcularfecha($i)
+        
+    }
+    
+  }
+  
+  protected function calcularFecha($dias){ 
+    $calculo = strtotime ("$dias days");
+    return date ("Y-m-d", $calculo);
   }
 }
--- apps/frontend/modules/alquileres/templates/indexSuccess.php
+++ apps/frontend/modules/alquileres/templates/indexSuccess.php
@@ -1,1 +1,69 @@
-cualquier cosa
+<div id="tabs">
+		
+  <ul>
+    <li><a href="#tabs-1">Cancha 1</br>(Futbol 5)</a></li>
+    <li><a href="#tabs-2">Cancha 2</br>(Futbol 5)</a></li>
+    <li><a href="#tabs-3">Cancha 3</br>(Futbol 8)</a></li>
+  </ul>
+  
+  <?php foreach ($valores_cancha as $numero => $archivo) :?>
+  <div id="tabs-<?php echo $numero; ?>">
+      
+    <img class="cancha" src="<?php echo image_path($archivo)?>" alt="pp"/>
+    
+    <?php for($i=0;$i<$dia_limite;$i++): ?>
+    <label style="color:yellow;margin-left:8em"><?php echo $fecha[$i];?></label>
+    <table class="alq">
+    
+    <form name="formulario" method="post" action="LogIn.html">
+      
+    <tr>
+        
+      <td class="alq">
+      <fieldset>
+        <label class= "option" for="localidad" style="font-size:23px">Horario:</br></br></label>
+        <select name="horarios" class="required">
+          <?php foreach($horario[$i] as $hora):?>                                          
+          <option value='<?php echo $hora?>'> <?php echo $hora.':00 hs'?> </option>                                              
+          <?php endforeach;?>
+       </select>
+     </fieldset>																
+     </td>		
+
+     <td class="alq">								
+       <fieldset>									
+          Servicios Adicionales<br>
+          <div style="width: auto; float: left">
+            <label class="opciones" for="op1">
+              <input class="opciones" type="checkbox" name="s1" id="op1" value="1"/>
+            </label>
+	    <br>
+	    <label class="opciones" for="op2">
+	      <input class="alq" type="checkbox" name="s2" id="op2" value="2"/>
+	    </label>
+	    <br>
+	    <label class="opciones" for="op3">
+	      <input class="alq" type="checkbox" name="s3" id="op3" value="3"/>
+	    </label>
+	  </div>
+	  <div style="text-align:left">-Indumentaria</div>
+	  <div style="text-align:left">-Ducha</div>
+	  <div style="text-align:left">-Confiteria</div>
+        </fieldset>
+      </td>	
+    
+      </tr>
+  
+      <td colspan="2" class="alq">					
+        <input class="submit" type="submit" value="Alquilar" onclick="return validar();" />
+      </td>
+						
+      </form>    
+        
+    </table>
+    <?php endfor ?>
+    
+  </div>  
+  <?php endforeach ?>
+    
+</div>
