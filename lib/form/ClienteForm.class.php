ef8373179b6545e4bef7e786e65954feff0a3dc6
matiasel20 <matiasel20@hotmail.com>
2012-06-26 16:49:45 -0300

    ABMs completos

--- lib/form/ClienteForm.class.php
+++ lib/form/ClienteForm.class.php
@@ -14,5 +14,13 @@ class ClienteForm extends BaseClienteForm
       unset ($this['equipoid']);
       $anios = range(date('Y') - 80, date('Y') - 18);
       $this->widgetSchema['fechanac']->setOption('years', $anios);
+      $this->widgetSchema['fechanac']->setOption('years', array_combine($anios, $anios));
+      //$this->widgetSchema->setHelp('dni','sin puntos'); // para añadir un comentario junto al input
+      $this->widgetSchema->setLabels(array(
+          'fechanac' => 'Fecha de nacimiento',
+          'user' => 'Nombre de usuario',
+          'telcel' => 'Tel/Cel',
+          'email' => 'E-mail'
+      ));
   }
 }
